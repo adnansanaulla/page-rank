07496b78298630e53029f2b15d58030b83defe4c
adnansanaulla <adnansanaulla7860@gmail.com>
2025-09-01 21:07:30 -0400

    minor changes

--- src/AdjacencyList.cpp
+++ src/AdjacencyList.cpp
@@ -10,31 +10,39 @@ using namespace std;
 AdjacencyList::AdjacencyList() {}
 
 // Method that adds a page to the graph
-void AdjacencyList::AddVertex(string to, string from, int& j) {
+void AdjacencyList::AddVertex(string to, string from, int &j)
+{
     int to2, find2;
-    if (mappar.find(to) == mappar.end()) {
+    if (mappar.find(to) == mappar.end())
+    {
         to2 = j;
         mappar[to] = j;
         reverse_mappar[j++] = to;
     }
-    else {
+    else
+    {
         to2 = mappar[to];
     }
-    if (mappar.find(from) == mappar.end()) {
+    if (mappar.find(from) == mappar.end())
+    {
         find2 = j;
         mappar[from] = j;
         reverse_mappar[j++] = from;
     }
-    else {
+    else
+    {
         find2 = mappar[from];
     }
-    if (adjacencyList.find(to2) == adjacencyList.end()) {
+    if (adjacencyList.find(to2) == adjacencyList.end())
+    {
         adjacencyList[to2] = {};
     }
-    if (adjacencyList.find(find2) == adjacencyList.end()) {
+    if (adjacencyList.find(find2) == adjacencyList.end())
+    {
         adjacencyList[find2] = {};
     }
-    if (outdegrees.size() < adjacencyList.size()) {
+    if (outdegrees.size() < adjacencyList.size())
+    {
         outdegrees.resize(adjacencyList.size(), 0);
     }
     adjacencyList[to2].push_back(find2);
@@ -42,32 +50,40 @@ void AdjacencyList::AddVertex(string to, string from, int& j) {
 }
 // prints the PageRank of all pages after p powerIterations in ascending
 // alphabetical order of webpages and rounding rank to two decimal places
-string AdjacencyList::PageRank(int n){
+string AdjacencyList::PageRank(int n)
+{
     stringstream ss;
     double temp;
-    vector<pair<string,int>> websites;
-    vector<double> ranks(static_cast<int>(adjacencyList.size()), static_cast<double>(1.0/adjacencyList.size()));
-    while (n > 1) {
+    vector<pair<string, int>> websites;
+    vector<double> ranks(static_cast<int>(adjacencyList.size()), static_cast<double>(1.0 / adjacencyList.size()));
+    while (n > 1)
+    {
         vector<double> newRanks(adjacencyList.size(), 0.0);
-        for (int i = 0; i < static_cast<int>(adjacencyList.size()); i++) {
-            for (int from: adjacencyList[i]) {
-                if (outdegrees[from] == 0) continue;
+        for (int i = 0; i < static_cast<int>(adjacencyList.size()); ++i)
+        {
+            for (int from : adjacencyList[i])
+            {
+                if (outdegrees[from] == 0)
+                    continue;
                 newRanks[i] += ranks[from] / outdegrees[from];
             }
         }
         ranks = newRanks;
         n--;
     }
-    for (size_t i = 0; i < ranks.size(); i++) {
+    for (size_t i = 0; i < ranks.size(); i++)
+    {
         websites.push_back({reverse_mappar[i], i});
     }
     sort(websites.begin(), websites.end());
-    for (size_t i = 0; i < websites.size(); i++) {
+    for (size_t i = 0; i < websites.size(); i++)
+    {
         temp = ranks[websites[i].second];
         ss << fixed << showpoint;
         ss << setprecision(2);
         ss << websites[i].first << " " << temp;
-        if (i != websites.size() - 1) {
+        if (i != websites.size() - 1)
+        {
             ss << endl;
         }
     }
